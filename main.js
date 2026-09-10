4355f8d1e12bcfaa5d82f8816846a8edca2fe0bd
Ken <Nebulocity@users.noreply.github.com>
2026-09-10 09:42:23 -0700

    Update main.js
    Loading Phaser from <script> instead of import.

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-import Phaser from 'phaser';
+// import Phaser from 'phaser';
+const Phaser = window.Phaser; // Load Phaser from <script> in index.html.
+
 import './style.css';
 import gameConfig from './config/gameConfig.js';
 

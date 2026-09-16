e2d2b4192aa309f02b00189466559fcf5c25de85
Ken <Nebulocity@users.noreply.github.com>
2026-09-16 14:26:42 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-import Phaser from 'phaser';
+// import Phaser from 'phaser';
+const Phaser = window.Phaser; // Phaser is loaded via <script> in index.html
 import './style.css';
 import gameConfig from './config/gameConfig.js';
 

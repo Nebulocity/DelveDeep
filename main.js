6439e1ad657e040ecd5ca70a34b63be1025befca
Nebulocity <ken.w.howard@gmail.com>
2026-09-16 16:57:18 -0700

    Packing Phaser into deployment for GitHub Pages.

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-// import Phaser from 'phaser';
-const Phaser = window.Phaser; // Phaser is loaded via <script> in index.html
+import Phaser from 'phaser';
 import './style.css';
 import gameConfig from './config/gameConfig.js';
 
